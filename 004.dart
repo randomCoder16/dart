56d66b350e38152c687cf46b9af27c9f54884e4a
randomCoder16 <elvistrevor67@gmail.com>
2026-09-15 19:29:46 +0300

    Maps

--- 004.dart
+++ 004.dart
@@ -11,4 +11,31 @@ void main(){
     print("shuffle: $numbers");
 
     Set<String> names = {"elvis", "trevor", "amunyo", "emma"};
+    print("\nSet: $names");
+    names.add("elvis");
+    print("add(elvis): $names");
+    names.remove("emma");
+    print("remove(emma): $names");
+    int length = names.length;
+    print("length: $length");
+
+    Map<int, String> planets = {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus",
+        8: "Neptune"
+    };
+
+    print("\nMap: $planets");
+    planets[9] = "Omicron";
+    print("planets[9]: $planets");
+    print("containsKey: ${planets.containsKey(99)}");
+    print("containsValue: ${planets.containsValue("Omicron")}");
+    print("remove(1): ${planets.remove(1)}");
+    print("planets: $planets");
+    print("values: ${planets.values}\nkeys: ${planets.keys}");
 }
