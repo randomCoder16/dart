95765ef3f792ae8050ef4fc4ff444ce2d45706e4
randomCoder16 <elvistrevor67@gmail.com>
2026-09-22 18:17:35 +0300

    basic math functions

---
+++ 010.dart
@@ -0,0 +1,4 @@
+// Re-do of classes...much more better tutorial
+
+void main() {
+}
---
+++ 011.dart
@@ -0,0 +1,17 @@
+// basic math functions
+
+import 'dart:math';
+
+void main() {
+
+    print("PI: $pi");
+
+    double sin_ = sin(45 * pi / 180);
+    print("sin: $sin_");
+
+    double cos_ = cos(135 * pi / 180);
+    print("cos: $cos_");
+
+    double squarert = sqrt(9);
+    print("Square-root (9): $squarert");
+}
